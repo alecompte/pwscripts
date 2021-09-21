9f3922afee5ef6f2b3cf5be4187be698b3ef6548
alecompte <38678863+alecompte@users.noreply.github.com>
2021-09-21 11:44:58 -0400

    Create ImportFileShares.ps1

---
+++ ImportFileShares.ps1
@@ -0,0 +1,124 @@
+###Import File Shares
+## Fonctionne avec ExportFileShares.ps1
+## Besoin de rehaussement
+
+$PathToCsv = "" ##Modifié juste ceci
+
+
+
+
+$Shares = Import-Csv $PathToCsv
+$ExistingShares = Get-SmbShare
+
+$Errors = @()
+
+
+foreach ($s in $Shares) {
+    if ($ExistingShares.Name -contains $s.Name) {
+        Write-Error $s.Name + "Already exists"
+    } else {
+        try {
+            $NewShare = New-SmbShare -Name $s.Name -Description $s.Description -Path $s.ConvertedPath
+        } catch {
+            $Error = [pscustomobject]@{
+                $State = 'Failed'
+                $ShareName = $s.Name
+                $SharePath = $s.Path
+                $Details = 'Failed to create share'
+            }
+            $Errors = $Errors + $Error
+        }
+        if ($NewShare) {
+            foreach ($p in $s.Permissions.Full) {
+                try {
+                    $NewShare | Grant-SmbShareAccess -AccountName $p -AccessRight Full
+                } catch {
+                    $Error = [pscustomobject]@{
+                        $State = 'FailedPermApply'
+                        $ShareName = $s.Name
+                        $SharePath = $s.Path
+                        $Details = ('Apply:'+$p)
+                    }
+                    $Errors = $Errors + $Error
+                }
+            }
+            foreach ($p in $s.Permissions.Change) {
+                try {
+                    $NewShare | Grant-SmbShareAccess -AccountName $p -AccessRight Change
+                } catch {
+                    $Error = [pscustomobject]@{
+                        $State = 'FailedPermApply'
+                        $ShareName = $s.Name
+                        $SharePath = $s.Path
+                        $Details = ('Apply:'+$p)
+                    }
+                    $Errors = $Errors + $Error
+                }
+            }
+
+            foreach ($p in $s.Permissions.Read) {
+                try {
+                    $NewShare | Grant-SmbShareAccess -AccountName $p -AccessRight Read
+                } catch {
+                    $Error = [pscustomobject]@{
+                        $State = 'FailedPermApply'
+                        $ShareName = $s.Name
+                        $SharePath = $s.Path
+                        $Details = ('Apply:'+$p)
+                    }
+                    $Errors = $Errors + $Error
+                }
+            }
+        
+            foreach ($p in $s.PermissionsDeny.Full) {
+                try {
+                    $NewShare | Block-SmbShareAccess -AccountName $p
+                } catch {
+                    $Error = [pscustomobject]@{
+                        $State = 'FailedPermApply'
+                        $ShareName = $s.Name
+                        $SharePath = $s.Path
+                        $Details = ('Apply:'+$p)
+                    }
+                    $Errors = $Errors + $Error
+                }
+        
+            }
+            foreach ($p in $s.Permissions.Change) {
+                try {
+                    $NewShare | Block-SmbShareAccess -AccountName $p
+                } catch {
+                    $Error = [pscustomobject]@{
+                        $State = 'FailedPermApply'
+                        $ShareName = $s.Name
+                        $SharePath = $s.Path
+                        $Details = ('Apply:'+$p)
+                    }
+                    $Errors = $Errors + $Error
+                }
+        
+            }
+            foreach ($p in $s.Permissions.Read) {
+                try {
+                    $NewShare | Block-SmbShareAccess -AccountName $p
+                } catch {
+                    $Error = [pscustomobject]@{
+                        $State = 'FailedPermApply'
+                        $ShareName = $s.Name
+                        $SharePath = $s.Path
+                        $Details = ('Apply:'+$p)
+                    }
+                    $Errors = $Errors + $Error
+                }
+            }
+
+        }
+
+    }
+
+
+}
+
+
+Write-Host "Write the path for the error report"
+Export-Csv $Errors -NoTypeInformation
