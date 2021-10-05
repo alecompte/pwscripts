8fb902beaaeb8ad0a20fff83e5ad668f22dec0b5
Antoine Lecompte <alecompte@natrix.info>
2021-10-05 10:26:27 -0400

    Add DisableOldAdUsers

---
+++ ActiveDirectory/DisableOldADUsers.ps1
@@ -0,0 +1,16 @@
+﻿$Days = 365
+
+$Users = Get-AdUser -Filter * -Properties LastLogonDate
+
+foreach ($u in $Users) {
+    
+    if (!$u.LastLogonDate) {
+        Write-Host $u.Name "Has never logged in"
+        Disable-AdAccount -Identity $u.DistinguishedName   
+    } elseif ((New-TimeSpan -Start ($u.LastLogonDate) -End (Get-Date)).Days -ge $Days) {
+    
+        Write-Host $u.Name "Last Logon Greater than 365 days"
+        Disable-AdAccount -Identity $u.DistinguishedName   
+    }
+
+}
